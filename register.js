1e7a3c1bbf7ea0f692c3e7c8580a8350ca3c49e0
SSgraal <67362989+SSgraal@users.noreply.github.com>
2023-11-19 11:13:06 +0200

    Add files via upload

--- register.js
+++ register.js
@@ -9,10 +9,37 @@ let messages = [];
 if(passwordText.value.length < 6)
     window.alert("the passwords don't match!!")
 
+var bipolarCheck = document.getElementById("bipolar");
+if(bipolarCheck.checked)
+    window.location.href="https://deadsimplechat.com/zzGIf-oJR";
+
 if(messages.length > 0)
 {
     e.preventDefault();
     errorElement.innerText = messages.join(', ');
 }
-
 });
+
+function changeSite()
+{
+    var bipolarCheck = document.getElementById("bipolar");
+    var schizoCheck = document.getElementById("schizo");
+    var schizoidCheck = document.getElementById("schizoid");
+    var autismCheck = document.getElementById("autism");
+    var ADDCheck = document.getElementById("ADD");
+    var BPDCheck = document.getElementById("BPD");
+    var DIDCheck = document.getElementById("DID");
+    var OCDCheck = document.getElementById("OCD");
+    var EDCheck = document.getElementById("ED");
+    var PTSDCheck = document.getElementById("PTSD");
+    var CPTSDCheck = document.getElementById("CPTSD");
+    var anxietyCheck = document.getElementById("anxiety");
+    if(bipolarCheck.checked)
+        window.location.href="https://deadsimplechat.com/zzGIf-oJR";
+    if(schizoCheck.checked)
+        window.location.href="https://deadsimplechat.com/0Sl29QA01";
+    if(ADDCheck.checked)
+        window.location.href="https://deadsimplechat.com/99GkJfU_S";
+    if(autismCheck.checked)
+        window.location.href="https://deadsimplechat.com/4sCZZoUiR";
+}
